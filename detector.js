c966cd0a5b6d74de347e6dbc759e06684a3689fc
kinsiabdirahman <kinsiabdirahman@outlook.com>
2023-12-17 23:21:55 +0300

    assignment completed

--- detector.js
+++ detector.js
@@ -1,21 +1,25 @@
 function speedDetector(speed) {
-    const speedLimit = 70;
-    const demeritPointsPerKmph = 5;
-    const maxDemeritPoints = 12;
-  
-    if (speed < speedLimit) {
-      console.log("Ok");
-      return;
-    }
-  
-    const demeritPoints = Math.floor((speed - speedLimit) / demeritPointsPerKmph);
-  
-    if (demeritPoints > maxDemeritPoints) {
-      console.log("License suspended");
-    } else {
-      console.log("Points: " + demeritPoints);
-    }
+  // Const for speed limits and demerit points
+  const speedLimit = 70;
+  const demeritPointsPerKmph = 5;
+  const maxDemeritPoints = 12;
+
+  // Check if the speed is below the speed limit
+  if (speed < speedLimit) {
+    console.log("Ok"); // Output "Ok" if within the speed limit
+    return;
   }
-  
-  speedDetector(200);
-  
+
+  // Calculate demerit points based on the difference between speed and speed limit
+  const demeritPoints = Math.floor((speed - speedLimit) / demeritPointsPerKmph);
+
+  // Check if demerit points exceed the maximum allowed
+  if (demeritPoints > maxDemeritPoints) {
+    console.log("License suspended"); // Output "License suspended" if demerit points exceed the maximum
+  } else {
+    console.log("Points: " + demeritPoints); // Output the calculated demerit points
+  }
+}
+
+// Test the function with a speed value of 200
+speedDetector(200);
--- grader.js
+++ grader.js
@@ -1,18 +1,20 @@
+// Function to calculate and return a grade based on  marks
 function calcGrade() {
-    var marks = 65;
-  
-    if (marks > 79) {
-      return "A";
-    } else if (marks >= 60 && marks <= 79) {
-      return "B";
-    } else if (marks >= 50 && marks <= 59) {
-      return "C";
-    } else if (marks >= 40 && marks <= 49) {
-      return "D";
-    } else {
-      return "E";
-    }
+  // Set the initial value for marks
+  var marks = 65;
+
+  if (marks > 79) {
+    return "A"; // Return 'A' if marks are greater than 79
+  } else if (marks >= 60 && marks <= 79) {
+    return "B"; // Return 'B' if marks are between 60 and 79
+  } else if (marks >= 50 && marks <= 59) {
+    return "C"; // Return 'C' if marks are between 50 and 59
+  } else if (marks >= 40 && marks <= 49) {
+    return "D"; // Return 'D' if marks are between 40 and 49
+  } else {
+    return "E"; // Return 'E' if marks are 40 or below
   }
-  
-  console.log(calcGrade());
-  
+}
+
+// Output the result of calcGrade function
+console.log(calcGrade());
